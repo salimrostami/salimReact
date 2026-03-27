6d2d967ed9a11328b35640af19da505f84d3f36b
salimrostami <salim.rostami@live.com>
2026-03-27 16:12:44 +0100

    small bug fix for title misalignment

--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -1,8 +1,8 @@
 const PageHeader = ({ title, description }) => (
-  <>
+  <div className="pageHeader">
     <p className="pageDescription">{description}</p>
     <h3 className="pageTitle">{title}</h3>
-  </>
+  </div>
 );
 
 export default PageHeader;
